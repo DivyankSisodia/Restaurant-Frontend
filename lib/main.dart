6ef57d0436810de0e512f791e7717d0739a6d9e3
Divyank Sisodia <Divyank1540.be20@chitkara.edu.in>
2024-05-15 15:02:37 +0530

    Added hive for storing user data

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:food_delivery/src/bottom_navbar.dart';
+import 'package:food_delivery/src/views/sign.screen.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -9,11 +12,22 @@ void main() async {
   await dotenv.load(fileName: ".env");
   SharedPreferences pref = await SharedPreferences.getInstance();
   String? token = pref.getString('token');
-  String? address = pref.getString('user_address');
-  runApp(ProviderScope(child: MyApp(token: token, initialAddress: address)));
+  await Hive.initFlutter();
+  await Hive.openBox('user_info');
+  SystemChrome.setEnabledSystemUIMode(
+    SystemUiMode.manual,
+    overlays: [SystemUiOverlay.bottom],
+  );
+  runApp(
+    ProviderScope(
+      child: MyApp(
+        token: token,
+      ),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   final String? token;
   final String? initialAddress;
 
@@ -24,14 +38,26 @@ class MyApp extends StatelessWidget {
   });
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void dispose() {
+    Hive.close();
+    super.dispose();
+  }
+  @override
   Widget build(BuildContext context) {
     // debugPrint('Token: $token');
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Your App',
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: const CustomBottomNavBar()
+      home: const CustomBottomNavBar(),
     );
   }
 }
--- lib/src/controller/login.controller.dart
+++ lib/src/controller/login.controller.dart
@@ -1,13 +1,18 @@
+// login_controller.dart
+
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart'; // Import flutter_riverpod
-import 'package:food_delivery/src/views/home.screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:food_delivery/src/bottom_navbar.dart';
+import 'package:hive/hive.dart';
 import 'package:http/http.dart' as http;
 import 'package:page_transition/page_transition.dart';
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import '../views/login.screen.dart';
 
 final loginProvider = ChangeNotifierProvider((ref) => LoginController());
 
@@ -15,9 +20,13 @@ class LoginController extends ChangeNotifier {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   late SharedPreferences pref;
+  late final Box box;
+  bool _isLoading = false; // Added isLoading variable
+  bool get isLoading => _isLoading;
 
   LoginController() {
     initSharedPref();
+    box = Hive.box('user_info'); // Initialize the box instance
   }
 
   void initSharedPref() async {
@@ -25,8 +34,31 @@ class LoginController extends ChangeNotifier {
   }
 
   Future<void> loginUser(BuildContext context) async {
+    // Toggle isLoading to true when login process starts
+    _isLoading = true;
+    notifyListeners();
+
     if (emailController.text.isEmpty || passwordController.text.isEmpty) {
       debugPrint('Email or password cannot be empty');
+      ScaffoldMessenger.maybeOf(context)?.showSnackBar(
+        SnackBar(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+          elevation: 10,
+          backgroundColor: Colors.redAccent.shade400,
+          content: const Text(
+            'Email or password cannot be empty',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+            ),
+          ),
+        ),
+      );
+      // Toggle isLoading back to false when login process ends
+      _isLoading = false;
+      notifyListeners();
       return;
     }
 
@@ -57,22 +89,28 @@ class LoginController extends ChangeNotifier {
 
       if (response.statusCode == 200) {
         var jsonRes = jsonDecode(response.body);
-
         if (jsonRes['success'] != null && jsonRes['success'] == true) {
           var myToken = jsonRes['token'] ?? '';
           pref.setString('token', myToken);
           debugPrint('TokenInfo: $myToken');
+
+          var user = jsonRes['user'];
+
+          var userAddress = user['address'];
+          box.put('address', userAddress);
+          // debugPrint('User Address: $userAddress');
+          var userProfile = user['profile'];
+          // debugPrint('User profile: $userProfile');
+          box.put('profilePic', userProfile);
 
           // Navigate to HomeScreen with the token and user data
           Navigator.push(
             context,
             PageTransition(
               duration: const Duration(milliseconds: 1200),
-              type: PageTransitionType.leftToRightWithFade,
-              child: HomeScreen(
-                token: myToken,
-                userData: jsonRes['user'],
-              ),
+              childCurrent: const LoginScreen(),
+              type: PageTransitionType.leftToRightJoined,
+              child: const CustomBottomNavBar(),
             ),
           );
         } else {
@@ -84,6 +122,17 @@ class LoginController extends ChangeNotifier {
       }
     } catch (error) {
       debugPrint('Error connecting: $error');
+    } finally {
+      // Toggle isLoading back to false when login process ends
+      _isLoading = false;
+      notifyListeners();
     }
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
   }
 }
